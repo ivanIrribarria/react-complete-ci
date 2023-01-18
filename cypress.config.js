b70a1f0a2efb17d5d58144db351ccd5a392e0684
Iván Irribarría <tyu7.91@gmial.com>
2023-01-18 14:48:30 +0100

    first ci try

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '97yid8',
+  projectId: "t29j1w",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
     },
   },
 });
